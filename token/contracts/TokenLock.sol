511503e379e8e94fb7172b8fec8251b358550e27
vincent <vencnte@gmail.com>
2020-09-26 15:03:23 +0800

    mint map toeken from tx unpack

--- token/contracts/TokenLock.sol
+++ token/contracts/TokenLock.sol
@@ -830,7 +830,7 @@ contract TokenGen {
 
     function unlock(bytes memory _proofData) public {
         (address _from, address _to, uint256 _value, bytes32 _tx, bytes memory _proof) = abi.decode(_proofData, (address, address, uint256, bytes32, bytes));
-        mapToken.mint(address(this), _value);
+        mapToken.mint(address(_from), _value);
     }
 }
 
